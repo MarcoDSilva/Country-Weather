3228068915adbf24e61be93a85e2854df971d57a
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-07-22 00:01:57 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      console.log("data obtained");
+      setCountries(res.data.map((country) => country.name));
+      console.log("data updated");
+    });
+  };
+
+  useEffect(hook, []);
+
+  const queryHandler = (e) => {
+    const val = e.target.value.toLowerCase();
+    console.log(val);
+    setFilter(val);
+    countryFilter();
+  };
+
+  const countryFilter = () => {
+    if (filter === "") {
+      return;
+    } else {
+      const search = countries.filter((c) => c.toLowerCase().includes(filter));
+      console.log(search);
+    }
+	};
+
+	const renderer = () => {
+		if(countries.length > 10) {
+			return ""
+		} else {
+			return countries.map((c, key) => <li key={key}>c</li>)
+		}
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries: <input value={filter} onChange={queryHandler} />
+      <ul>
+				 {renderer() == "" ? "Too many matches" : renderer().map((c,i) => <li key={i}>{c}</li>)}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
