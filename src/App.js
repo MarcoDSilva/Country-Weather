bbfb7418827ca79606f2481bd89775384484b20d
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-07-24 23:44:25 +0100

    ternary conditional to filter, created Country Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const Country = ({ filter }) => {
+  const country = filter();
+
+  return (
+    <div key={0}>
+      <h1>{country[0]["name"]}</h1>
+      <p>Population: {country[0]["population"]}</p>
+      <p>Capital: {country[0]["capital"]}</p>
+      <h4>Languages: </h4>
+      <ul>
+        {country[0]["languages"].map((lang, i) => (
+          <li key={i}>{lang["name"]}</li>
+        ))}
+      </ul>
+      <p>{<img src={country[0]["flag"]} />}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState("");
@@ -11,10 +30,11 @@ const App = () => {
       console.log("data obtained");
       setCountries(res.data);
       console.log("data updated");
+      console.log(res.data);
     });
   };
 
-  useEffect(hook, []); // applies the api data 
+  useEffect(hook, []); // applies the api data
 
   // handles the filter input box
   const queryHandler = (e) => {
@@ -23,32 +43,32 @@ const App = () => {
 
   //filter the data by the names
   const countryFilter = () => {
-    if (filter === "") {      
+    if (filter === "") {
       return countries;
     } else {
-      let arr = []
+      let arr = [];
 
-      for(let obj in countries) {
-        if(countries[obj]["name"].toLowerCase().includes(filter)) {
-          arr.push(countries[obj]["name"])
+      for (let obj in countries) {
+        if (countries[obj]["name"].toLowerCase().includes(filter)) {
+          arr.push(countries[obj]);
         }
       }
 
-
-
       console.log(arr);
-      return arr
+      return arr;
     }
   };
 
   return (
     <div>
       Find countries: <input value={filter} onChange={queryHandler} />
-      <ul>
-        {countryFilter().length > 10
-          ? "Too many matches, specify better the filter"
-          : countryFilter().map((c, i) => <li key={i}>{c}</li>)}
-      </ul>
+      {countryFilter().length > 10 ? (
+        <p>Too many matches, specify better the filter</p>
+      ) : countryFilter().length === 1 ? (
+        <Country filter={countryFilter} />
+      ) : (
+        countryFilter().map((c, i) => <p key={i}>{c["name"]}</p>)
+      )}
     </div>
   );
 };
